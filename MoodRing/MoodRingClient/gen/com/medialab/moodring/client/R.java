2a7d4b8b805203af08ccf323f63aec010c2c27cf
Erez <erezfarhan@gmail.com>
2013-02-09 14:05:10 +0200

    Removing /gen stuf from git
    In addition to being put in git ignore, they sould also be removed
    because they have been committed before

--- MoodRing/MoodRingClient/gen/com/medialab/moodring/client/R.java
+++ MoodRing/MoodRingClient/gen/com/medialab/moodring/client/R.java
@@ -14,20 +14,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_register=0x7f060002;
-        public static final int button_send=0x7f060007;
-        public static final int edittext_message=0x7f060006;
-        public static final int edittext_username=0x7f060001;
-        public static final int listview_users=0x7f060004;
-        public static final int registration_layout=0x7f060000;
-        public static final int textview_selected_to=0x7f060005;
-        public static final int work_layout=0x7f060003;
+        public static final int button_friends=0x7f070003;
+        public static final int button_register=0x7f070004;
+        public static final int button_send=0x7f070008;
+        public static final int edittext_message=0x7f070007;
+        public static final int edittext_username=0x7f070001;
+        public static final int home_layout=0x7f070002;
+        public static final int listview_users=0x7f070005;
+        public static final int menu_settings=0x7f070009;
+        public static final int registration_layout=0x7f070000;
+        public static final int textview_selected_to=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_user_list=0x7f030001;
+    }
+    public static final class menu {
+        public static final int activity_user_list=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
+        public static final int title_activity_user_list=0x7f040003;
     }
     public static final class style {
         /** 
