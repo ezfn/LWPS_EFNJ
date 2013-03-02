8522e2f8fa3a7f6df5e7a7bd50c094f8b458bc30
Erez Farhan <erezfarhan@gmail.com>
2013-03-03 01:26:30 +0200

    Revert "friends report fixed stressed conditions"
    This reverts commit 484dca77651eb541774ac9027dace84973b9179b.

--- MoodRing/MoodRingClient/src/com/medialab/moodring/client/UserListActivity.java
+++ MoodRing/MoodRingClient/src/com/medialab/moodring/client/UserListActivity.java
@@ -1,13 +1,10 @@
 package com.medialab.moodring.client;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -44,33 +41,39 @@ public class UserListActivity extends Activity {
 						ArrayAdapter<String> adapter = new ArrayAdapter<String>(UserListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, users);
 						ListView listView = (ListView) findViewById(R.id.listview_users);
 						listView.setAdapter(adapter);
+						final TextView textView = (TextView) findViewById(R.id.textview_selected_to);
 						listView.setOnItemClickListener(new OnItemClickListener() {
+
 							@Override
 							public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-								final String selected = users[arg2];
-								OnClickListener voteListener = new OnClickListener(){
+								String selected = users[arg2];
+								textView.setText(selected);
+							}
+						});
 
-									@Override
-									public void onClick(DialogInterface dialog, final int which) {
-										
-										new Thread(new Runnable() {
-											public void run() {
-												String res = ServerCalls.sendMessage(selected, getCurrentUserId(), Integer.toString(which));
-												System.out.println(res);
-											}
-										}).start();
-										
-										Toast.makeText(UserListActivity.this, "Thanks!", Toast.LENGTH_SHORT).show();								
+						findViewById(R.id.button_send).setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+								EditText messageEditText = (EditText) findViewById(R.id.edittext_message);
+								final String body = messageEditText.getText().toString();
+								if (body == null || body.length() < 1) {
+									Toast.makeText(UserListActivity.this, "Message is empty", Toast.LENGTH_SHORT).show();
+									return;
+								}
+
+								final String to = textView.getText().toString();
+								if (to == null || to.length() < 1) {
+									Toast.makeText(UserListActivity.this, "No recipient specified", Toast.LENGTH_SHORT).show();
+									return;
+								}
+
+								new Thread(new Runnable() {
+									public void run() {
+										String res = ServerCalls.sendMessage(to, getCurrentUserId(), body);
+										System.out.println(res);
 									}
-								};
-								Builder builder = new AlertDialog.Builder(UserListActivity.this);
-								AlertDialog dialog = builder.create();	
-								dialog.setTitle("Would you say " + selected + " is:");
-								dialog.setButton(AlertDialog.BUTTON_NEGATIVE, (CharSequence)"Relaxed", voteListener);
-								dialog.setButton(AlertDialog.BUTTON_NEUTRAL, (CharSequence)"Neutral", voteListener);
-								dialog.setButton(AlertDialog.BUTTON_POSITIVE, (CharSequence)"Stressed", voteListener);
-								dialog.setCancelable(true);
-								dialog.show();
+								}).start();
 							}
 						});
 					}
